85810823e2a0a6d1e47d1559eba33ef9b95ac543
aa638 <alfred.tovsen.mo@intility.no>
2024-08-16 12:35:54 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nkzk/example-project
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello world!"))
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+
+}
